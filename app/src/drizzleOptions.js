4aa5656baa385520524c7b7167ad11a1c45c0a14
Alex Fisher <alexanderfisher@gmail.com>
2020-05-10 15:40:11 -0400

    Removed extraneous semicolon in drizzleOptions comment.

--- app/src/drizzleOptions.js
+++ app/src/drizzleOptions.js
@@ -9,7 +9,7 @@ const options = {
   web3: {
     block: false,
     // REMOVED BELOW LINE so Drizzle uses Metamask (wasted too much time realizing this is what I had to do)
-    //customProvider: new Web3("ws://localhost:8545");
+    //customProvider: new Web3("ws://localhost:8545")
   },
   contracts: [SimpleStorage, ComplexStorage, TutorialToken, SuicideKings],
   events: {
