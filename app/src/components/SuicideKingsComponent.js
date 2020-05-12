9600372db9709204c0ae5eccf0d074ac39ca2202
Alex Fisher <alexanderfisher@gmail.com>
2020-05-12 17:59:17 -0400

    Added a List Kings Component and did some basic CSS styling.

--- app/src/components/SuicideKingsComponent.js
+++ app/src/components/SuicideKingsComponent.js
@@ -1,45 +1,31 @@
 import React from "react";
 import { newContextComponents } from "@drizzle/react-components";
 import AccountInfoComponent from "./AccountInfoComponent";
+import ListKingsComponent from "./ListKingsComponent";
 import logo from "../logo.png";
 
 //const { AccountData, ContractData, ContractForm } = newContextComponents;
-const { ContractData, ContractForm } = newContextComponents;
+const { ContractForm } = newContextComponents;
 
 const SuicideKingsComponent = ({ drizzle, drizzleState }) => {
   return (
     <div className="App">
 
-      <div>
-        <img src={logo} alt="suicidekings-logo" />
-        <h1>Suicide Kings</h1>
+      <div className="AppHeader">
+        <div className="AppTitle">
+          <img src={logo} alt="suicidekings-logo" />
+          <h1>Suicide Kings</h1>
+          <AccountInfoComponent drizzle={drizzle} drizzleState={drizzleState} />
+        </div>
       </div>
-
-      <AccountInfoComponent drizzle={drizzle} drizzleState={drizzleState} />
-
-      <div className="section">
-        <h2>SuicideKings</h2>
-        <p>
-          <strong>Stored Value: </strong>
-          <ContractData
-            drizzle={drizzle}
-            drizzleState={drizzleState}
-            contract="SuicideKings"
-            method="getValue"
-          />
-        </p>
-        <ContractForm drizzle={drizzle} contract="SuicideKings" method="setValue" />
-        <ContractForm drizzle={drizzle} contract="SuicideKings" method="mintKing" sendArgs={{gas: 800000}} />
-        <p>
-          <strong>King Count: </strong>
-          <ContractData
-            drizzle={drizzle}
-            drizzleState={drizzleState}
-            contract="SuicideKings"
-            method="balanceOf"
-            methodArgs={[drizzleState.accounts[0]]}
-          />
-        </p>        
+      <div className="AppMain">
+        <div className="MintKing">
+        <div className="ComponentTitle">Mint King: </div>
+          <ContractForm drizzle={drizzle} contract="SuicideKings" method="mintKing" sendArgs={{gas: 800000}} />      
+        </div>
+        <div className="ListKings">
+          <ListKingsComponent drizzle={drizzle} drizzleState={drizzleState} />
+        </div>
       </div>
 
     </div>
