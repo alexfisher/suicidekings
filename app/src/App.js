2bc10e75ac58a4b42ed292ba71514e670d95395f
Kevin Vitale <kevinvitale@gmail.com>
2020-05-20 11:18:13 -0400

    [WEB] adds 'AppLayout' component
    Again, this is being piecemealed into our app from the
    "Charged Particle" project

--- app/src/App.js
+++ app/src/App.js
@@ -4,6 +4,10 @@ import { DrizzleContext } from "@drizzle/react-plugin";
 import { Drizzle } from "@drizzle/store";
 import drizzleOptions from "./drizzleOptions";
 import SuicideKingsComponent from "./components/SuicideKingsComponent";
+
+// Page Templates
+import AppLayout from './layout/AppLayout';
+
 import logo from "./logo.png";
 import "./App.css";
 
@@ -11,29 +15,31 @@ const drizzle = new Drizzle(drizzleOptions);
 
 const App = () => {
   return (
-    <DrizzleContext.Provider drizzle={drizzle}>
-      <DrizzleContext.Consumer>
-        {drizzleContext => {
-          const { drizzle, drizzleState, initialized } = drizzleContext;
+    <AppLayout>
+      <DrizzleContext.Provider drizzle={drizzle}>
+        <DrizzleContext.Consumer>
+          {drizzleContext => {
+            const { drizzle, drizzleState, initialized } = drizzleContext;
 
-          window.drizzle = drizzle;
-          window.drizzleState = drizzleState;
+            window.drizzle = drizzle;
+            window.drizzleState = drizzleState;
 
-          if (!initialized) {
+            if (!initialized) {
+              return (
+                <div className="Loading">
+                  <img src={logo} alt="suicidekings-logo" />
+                  <div>Loading...</div>
+                </div>
+              )
+            }
+
             return (
-              <div className="Loading">
-                <img src={logo} alt="suicidekings-logo" />
-                <div>Loading...</div>
-              </div>
+              <SuicideKingsComponent drizzle={drizzle} drizzleState={drizzleState} />
             )
-          }
-
-          return (
-            <SuicideKingsComponent drizzle={drizzle} drizzleState={drizzleState} />
-          )
-        }}
-      </DrizzleContext.Consumer>
-    </DrizzleContext.Provider>
+          }}
+        </DrizzleContext.Consumer>
+      </DrizzleContext.Provider>
+    </AppLayout>
   );
 }
 
