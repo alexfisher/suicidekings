48481471e54937cfd8824189fa3a3e882b426d98
Alex Fisher <alexanderfisher@gmail.com>
2020-05-20 17:06:15 -0400

    [UI] Initial integration of material UI along with override of entire theme's styling.

--- app/src/App.js
+++ app/src/App.js
@@ -1,39 +1,68 @@
 //import Web3 from "web3";
 import React from "react";
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import ProTip from './ProTip';
 import { DrizzleContext } from "@drizzle/react-plugin";
 import { Drizzle } from "@drizzle/store";
 import drizzleOptions from "./drizzleOptions";
 import SuicideKingsComponent from "./components/SuicideKingsComponent";
 import logo from "./logo.png";
-import "./App.css";
+//import "./App.css";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        The Crypto Bros
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 const drizzle = new Drizzle(drizzleOptions);
 
 const App = () => {
   return (
-    <DrizzleContext.Provider drizzle={drizzle}>
-      <DrizzleContext.Consumer>
-        {drizzleContext => {
-          const { drizzle, drizzleState, initialized } = drizzleContext;
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Suicide Kings
+        </Typography>
+      </Box>
+      <Box>
+        <DrizzleContext.Provider drizzle={drizzle}>
+          <DrizzleContext.Consumer>
+            {drizzleContext => {
+              const { drizzle, drizzleState, initialized } = drizzleContext;
 
-          window.drizzle = drizzle;
-          window.drizzleState = drizzleState;
+              window.drizzle = drizzle;
+              window.drizzleState = drizzleState;
 
-          if (!initialized) {
-            return (
-              <div className="Loading">
-                <img src={logo} alt="suicidekings-logo" />
-                <div>Loading...</div>
-              </div>
-            )
-          }
+              if (!initialized) {
+                return (
+                  <div className="Loading">
+                    <img src={logo} alt="suicidekings-logo" />
+                    <div>Loading...</div>
+                  </div>
+                )
+              }
 
-          return (
-            <SuicideKingsComponent drizzle={drizzle} drizzleState={drizzleState} />
-          )
-        }}
-      </DrizzleContext.Consumer>
-    </DrizzleContext.Provider>
+              return (
+                <SuicideKingsComponent drizzle={drizzle} drizzleState={drizzleState} />
+              )
+            }}
+          </DrizzleContext.Consumer>
+        </DrizzleContext.Provider>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
   );
 }
 
--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
+import theme from './theme';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
+  document.querySelector('#root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
