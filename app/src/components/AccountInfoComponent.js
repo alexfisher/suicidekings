56efa321d9ae727c9b4037f4f548201a1ca090d5
Alex Fisher <alexanderfisher@gmail.com>
2020-05-20 10:35:15 -0400

    Modified AccountInfoComponent to be a class and pull in KING symbol.

--- app/src/components/AccountInfoComponent.js
+++ app/src/components/AccountInfoComponent.js
@@ -3,7 +3,58 @@ import { newContextComponents } from "@drizzle/react-components";
 
 const { AccountData, ContractData } = newContextComponents;
 
-const AccountInfoComponent = ({ drizzle, drizzleState }) => {
+class AccountInfoComponent extends React.Component {
+  state = { dataKey: null };
+
+  componentDidMount() {
+    // Destructure properties of the component 
+    // (at least drizzle and drizzleState will be passed in JSX call, but you can add more)
+    const { drizzle } = this.props;
+    const SuicideKing = drizzle.contracts.SuicideKing;
+    const dataKey = SuicideKing.methods["symbol"].cacheCall();
+    this.setState({dataKey});
+    
+    // We can print our drizzle objects to the console if needed!
+    // Or, just install React Developer Tools to your browser and view Components > props
+    //console.log(drizzle);
+    //console.log(drizzleState);
+  }
+
+  render() {
+    const { drizzle, drizzleState } = this.props;
+    const { SuicideKing } = drizzleState.contracts;
+    const symbol = SuicideKing.symbol[this.state.dataKey];
+
+    return (
+      <div className="AccountInfo">
+        <div className="AccountBalance">
+          <AccountData
+            drizzle={this.props.drizzle}
+            drizzleState={this.props.drizzleState}
+            accountIndex={0}
+            units="ether"
+            precision={3}
+          />
+        </div>      
+        <div className="KingBalance">
+          Total Supply:&nbsp;   
+          <ContractData
+            drizzle={this.props.drizzle}
+            drizzleState={this.props.drizzleState}
+            contract="SuicideKing"
+            method="totalSupply"
+            methodArgs={[2]}
+          />&nbsp;{symbol && symbol.value}S
+          
+        </div>
+      </div>
+    )
+  }
+
+} 
+
+/*
+({ drizzle, drizzleState }) => {
   return (
     <div className="AccountInfo">
       <div className="AccountBalance">
@@ -16,27 +67,19 @@ const AccountInfoComponent = ({ drizzle, drizzleState }) => {
         />
       </div>      
       <div className="KingBalance">
-        Total Supply:&nbsp;
+        Total Supply:&nbsp;   
         <ContractData
           drizzle={drizzle}
           drizzleState={drizzleState}
           contract="SuicideKing"
           method="totalSupply"
-          methodArgs={[1]}
+          methodArgs={[2]}
         />
-        &nbsp;KINGS
+        &nbsp;KINGS 
       </div>
-    {/*
-      <div>
-        Web3 Connection Status: {drizzleState.drizzleStatus.initialized.toString()}
-      </div>
-      <div>
-        Web3 Connection Status: {drizzleState.drizzleStatus.initialized ? 'online' : 'offline'}
-      </div>          
-    */}
-
     </div>
   );
 };
+*/
 
 export default AccountInfoComponent;
